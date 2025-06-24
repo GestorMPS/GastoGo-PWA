ec1c685edeef7320e7916cb75c25d6d061c69065
GestorMPS <mponcesaravia@gmail.com>
2025-06-24 14:49:24 -0300

    Update 2 tarjetas.js

--- assets/js/tarjetas.js
+++ assets/js/tarjetas.js
@@ -179,16 +179,13 @@ tarjetas.forEach(t => {
   [inputEntidad, inputAlias, inputCierre].forEach(el => el.addEventListener('input', toggleBtnTarj));
   toggleBtnTarj();
 
-    btnGuardarTarj.addEventListener('click', () => {
-    const ent = inputEntidad.value.trim();
-    const alias = inputAlias.value.trim();
-    const dia = inputCierre.value;
-    const msg = `¿Crear tarjeta "${alias} (${ent})" con cierre día ${dia}?`;
-    if (!confirm(msg)) return;
-     // resto del código…
-   });
-
-    
+  btnGuardarTarj.addEventListener('click', () => {
+    tarjetas.push({
+      id: Date.now(),
+      entidad: inputEntidad.value.trim(),
+      alias: inputAlias.value.trim(),
+      diaCierre: +inputCierre.value
+    });
     inputEntidad.value = '';
     inputAlias.value = '';
     inputCierre.value = '';
@@ -291,5 +288,3 @@ tarjetas.forEach(t => {
   renderTarjetas();
   renderGastos();
 });
-
-
